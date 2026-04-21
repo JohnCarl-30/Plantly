2cfb6dd32e162201ff057440f90150997f668f87
John Carl <johncarlsantos30@gmail.com>
2026-04-21 16:23:56 +0800

    async storage

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,11 +1,24 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 type UserStore = {
-    hasFinishedOnboarding: boolean;
-    toggleOnboarding: () => void;
+  hasFinishedOnboarding: boolean;
+  toggleOnboarding: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-    hasFinishedOnboarding: false,
-    toggleOnboarding: () => set((state) => ({hasFinishedOnboarding: !state.hasFinishedOnboarding})),
-}));
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleOnboarding: () =>
+        set((state) => ({
+          hasFinishedOnboarding: !state.hasFinishedOnboarding,
+        })),
+    }),
+    {
+      name: 'plantly-user-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
